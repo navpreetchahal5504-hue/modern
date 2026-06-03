b0c37d8b55f02c5dcca78d622e1c75567f184b2c
navpreetchahal5504-hue <navpreetchahal5504@gmail.com>
2026-06-02 23:14:14 -0400

    Refactor output messages in script.js

--- script.js
+++ script.js
@@ -10,22 +10,23 @@ if (health < 30 && enemyNearby === true) {
     output.textContent = "CRITICAL ALERT: Immediate Evacuation Required";
 }
 
-else if (ammo < 5 || shield === false) {
-    output.textContent = "Warning: Low Resources";
+else if (missionProgress === 100) {
+    output.textContent = "Mission Complete Successfully";
 }
 
-else if (missionProgress >= 1 && missionProgress <= 70) {
-    output.textContent = "Mission In Progress";
+else if (ammo < 5 || shield === false) {
+    output.textContent = "Warning: Low Resources";
 }
 
 else if (missionProgress > 70 && enemyNearby === false) {
     output.textContent = "Approaching Mission Completion";
 }
 
-else if (missionProgress === 100) {
-    output.textContent = "Mission Complete Successfully";
+else if (missionProgress >= 1 && missionProgress <= 70) {
+    output.textContent = "Mission In Progress";
 }
 
 else {
     output.textContent = "System Stable";
 }
+    
